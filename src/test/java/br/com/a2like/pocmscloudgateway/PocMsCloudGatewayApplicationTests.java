ac6939959e6cf65c4fd78e46e9f39a466dcb4c29
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-09-11 18:35:33 -0300

    Criando microservice Cloud Gateway

---
+++ src/test/java/br/com/a2like/pocmscloudgateway/PocMsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.a2like.pocmscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMsCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
